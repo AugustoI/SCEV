fc8b5cd7844775b333a074fc0d8554f473542634
João Couth <craftcouth@gmail.com>
2018-07-08 20:32:36 -0300

    ConexaoDAO e VendasDAO

---
+++ src/DAO/VendasDAO.java
@@ -0,0 +1,17 @@
+/*
+ * Classe VendasDAO
+ *
+ * Classe utilizada para controle das vendas, contendo
+ * metodos utilitarios referentes ao banco de dados.
+ *
+ * Versao: 1.0.0
+ */
+package DAO;
+
+/**
+ *
+ * @author Couth
+ */
+public class VendasDAO {
+    
+}
