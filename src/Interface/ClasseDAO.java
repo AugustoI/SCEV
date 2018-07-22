d9497f8137d97de5853d30ab71447526df7169f1
GustavoAugusto <gustavo.augusto.pires@hotmail.com>
2018-07-21 21:20:31 -0300

    ,

--- src/Interface/ClasseDAO.java
+++ src/Interface/ClasseDAO.java
@@ -10,9 +10,16 @@ package Interface;
  * @author ACER
  */
 public interface ClasseDAO {
+    /*
+    *Esses sãos os métodos báscios que toda classe de conexão com o banco deveria ter
+    */
     public boolean Inserir();
     public boolean Editar();
     public boolean Deletar();
     public boolean Pesquisar();
+    /*
+    *Esse método limpa os atributos da classe
+    */
     public void Limpar();
+    
 }
---
+++ src/Interface/EntidadesBase.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author ACER
+ */
+public interface EntidadesBase {
+
+    /**
+     *Obrigatório que toda classe base coloque o tipo, que será a restrilção da tabela para sempre.
+     */
+    public void SetTipo();
+    
+}
